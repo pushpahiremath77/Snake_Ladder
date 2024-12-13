3b7329c6779eccd772b93a248d7d4a8e24aaac92
pushpahiremath77 <pushpahiremath20@gmail.com>
2024-12-13 14:29:38 +0530

    Use case-2 to get random integers

--- snake_ladder.py
+++ snake_ladder.py
@@ -1,5 +1,14 @@
-def play_uc1():
-    position = 0
-    print(f"Player starts at position {position}")
+#use case 1
+# def play_uc1():
+#     position = 0
+#     print(f"Player starts at position {position}")
     
-play_uc1()
+# play_uc1()
+
+
+
+#use case 2
+import random
+def roll_die():
+    print(f"Player rolled: {random.randint(1, 6)}")
+roll_die()
