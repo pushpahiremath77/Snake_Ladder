76b365b41b9736448425945b405a717abdbb1c28
pushpahiremath77 <pushpahiremath20@gmail.com>
2024-12-13 14:35:46 +0530

    Use case-4:The game should repeat till position 100

--- snake_ladder.py
+++ snake_ladder.py
@@ -1,42 +1,32 @@
-#use case 1
-# def play_uc1():
-#     position = 0
-#     print(f"Player starts at position {position}")   
-# play_uc1()
-
-#use case 2
-# import random
-# def roll_die():
-#     print(f"Player rolled: {random.randint(1, 6)}")
-# roll_die()
-
-#use case 3
 import random
-def roll_die():
-    return random.randint(1, 6)
-
-def get_option():
-    option = ['NO_PLAY', 'LADDER', 'SNAKE']
-    return random.choice(option)
-    
-def uc3():
+def repeat_roll():
     position=0
-    die=roll_die()
-    choice=get_option()
-    
-    print(f"Die rolled: {die}")
-    if choice=='LADDER':
-        position+=die
-        print(f"{choice}! The player move ahead to position {position}")
-    elif choice=='SNAKE':
-        position-=die
-        if position<0:
-            position=0
-        print(f"{choice}! The player move behind to the position {position}")
-    else:
-        print(f"{choice}! The player stays in the same position")
+    while(position!=100):
+        dice=random.randint(1,6)
+        choice=random.randint(1,3)
 
-uc3()
+        if choice==1:
+            if position + dice <= 100:
+                position+=dice
+                print(f"Rolled {dice}, moved forward. Position: {position}")
+            else:
+                print(f"Rolled {dice}, but move exceeds 100. Staying at position: {position}")
+        
+        elif choice==2:
+            position-=dice
+
+            if position<0:
+                position=0
+            print(f"Rolled {dice}, moved backward. Position: {position}")
+
+        else:
+            print(f"Staying in same position: {position}")
+    print(f"Player reached the winning position: {position}")
+
+repeat_roll()
+
+
+
 
 
 
