0646cdbf4580f2c4e7da926b5e99cb0e181fb67a
pushpahiremath77 <pushpahiremath20@gmail.com>
2024-12-17 17:48:36 +0530

    Added code for play with 2 players

--- snake_ladder.py
+++ snake_ladder.py
@@ -55,40 +55,4 @@ play_two_players()
 
 
 
-    # while player1_position < 100 and player2_position < 100:
-    #     if turn == 1:
-    #         die_value = roll_die()
-    #         rolls1 += 1
-    #         option = check_option(die_value)
-            
-    #         if option == "Ladder" and player1_position + die_value <= 100:
-    #             player1_position += die_value
-    #         elif option == "Snake":
-    #             player1_position -= die_value
-    #             if player1_position < 0:
-    #                 player1_position = 0
-            
-    #         print(f"Player 1's position: {player1_position}")
-    #         if player1_position == 100:
-    #             print(f"Player 1 wins after {rolls1} rolls!")
-    #             break
-    #         turn = 2 
-    #     else:
-    #         die_value = roll_die()
-    #         rolls2 += 1
-    #         option = check_option(die_value)
-            
-    #         if option == "Ladder" and player2_position + die_value <= 100:
-    #             player2_position += die_value
-    #         elif option == "Snake":
-    #             player2_position -= die_value
-    #             if player2_position < 0:
-    #                 player2_position = 0
-            
-    #         print(f"Player 2's position: {player2_position}")
-    #         if player2_position == 100:
-    #             print(f"Player 2 wins after {rolls2} rolls!")
-    #             break
-    #         turn = 1
-
-
+    
